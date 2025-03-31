385cd7703774a9ea6bdc6eb182f112702f4eca83
tera2839 <112415115+tera2839@users.noreply.github.com>
2025-03-31 13:57:46 +0900

    Update AdminController.java

--- src/main/java/com/quickshift/controller/AdminController.java
+++ src/main/java/com/quickshift/controller/AdminController.java
@@ -428,7 +428,7 @@ public class AdminController {
 		Store store = aService.findByStoreId(max);
 		Long id = store.getId();
 		
-		String url = requestUrl + "/memberLogin?store_id=" + id;
+		String url = requestUrl + "/memberLogin?storeId=" + id;
 		aService.updateStoreUrl(id, url);
 		
 		for(int i = 0; i < 15; i++) {
